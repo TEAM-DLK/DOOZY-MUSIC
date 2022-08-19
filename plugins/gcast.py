f307e3ff8bc5ff8d6e990f3819d1538d4d9decb6
TEAM DLK <93440107+TEAM-DLK@users.noreply.github.com>
2022-08-20 04:41:15 +0530

    Update gcast.py

--- plugins/gcast.py
+++ plugins/gcast.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2021 By AdityaPlayer
+# Copyright (C) 2021 By DoozyPlayer
 
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Dialog, Chat, Message
 from pyrogram.errors import UserAlreadyParticipant
-from modules.clientbot.clientbot import client as aditya
+from modules.clientbot.clientbot import client as doozy
 from modules.config import SUDO_USERS
 
 @Client.on_message(filters.command(["gcast", "post", "send"]))
@@ -19,9 +19,9 @@ async def broadcast(_, message: Message):
             await wtf.edit("**__Ƥɭɘɑsɘ Ʀɘƥɭy Ƭø ɑ Mɘssɑʛɘ Ƭø Stɑɤt Ɓɤøɑɗƈɑst ...__**")
             return
         lmao = message.reply_to_message.text
-        async for dialog in aditya.iter_dialogs():
+        async for dialog in doozy.iter_dialogs():
             try:
-                await aditya.send_message(dialog.chat.id, lmao)
+                await doozy.send_message(dialog.chat.id, lmao)
                 sent = sent+1
                 await wtf.edit(f"`Ɓɤøɑɗƈɑstɩŋʛ` \n\n**Sɘŋt Ƭø:** `{sent}` Ƈɦɑts \n**Fɑɩɭɘɗ Iŋ:** {failed} chats")
                 await asyncio.sleep(3)
