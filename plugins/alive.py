490a0878be6c3cfc13dea90e5fc0715e06d75665
TEAM DLK <93440107+TEAM-DLK@users.noreply.github.com>
2022-08-24 18:10:57 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -5,35 +5,35 @@ from modules.helpers.filters import command
 from modules.helpers.command import commandpro
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-
+from modules.config import OWNER_USERNAME, SUPPORT_GROUP, SUPPORT_CHANNEL
 
 START_TIME = datetime.utcnow()
 START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
 TIME_DURATION_UNITS = (
-    ('week', 60 * 60 * 24 * 7),
-    ('day', 60 * 60 * 24),
-    ('hour', 60 * 60),
-    ('min', 60),
-    ('sec', 1)
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
 )
 
 async def _human_time_duration(seconds):
-    if seconds == 0:
-        return 'inf'
-    parts = []
-    for unit, div in TIME_DURATION_UNITS:
-        amount, seconds = divmod(int(seconds), div)
-        if amount > 0:
-            parts.append('{} {}{}'
-                         .format(amount, unit, "" if amount == 1 else "s"))
-    return ', '.join(parts)
-    
-   
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+    
+   
 
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
-    await message.reply_photo(
-        photo=f"https://te.legra.ph/file/c43ae4bf7d12ce40e76e8.jpg",
+    await message.reply_photo(
+        photo=f"https://te.legra.ph/file/c43ae4bf7d12ce40e76e8.jpg",
         caption=f"""**━━━━━━━━━━━━━━━━━━━━━━━━
 🇱🇰☆° ﾟ𝗛𝗲𝗹𝗹𝗼
 𝗜 𝗔𝗠 𝗣𝗟𝗔𝗬𝗜𝗡𝗚 𝗠𝗨𝗦𝗜𝗖 𝗦𝗢𝗡𝗚𝗦 𝗜𝗡 𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 𝗩𝗜𝗗𝗘𝗢 𝗖𝗛𝗔𝗧.❤️
@@ -42,11 +42,11 @@ async def start_(client: Client, message: Message):
 ""/help"" - 𝗖𝗛𝗔𝗧𝗕𝗢𝗧 𝗛𝗘𝗟𝗣 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦
 ""/on"" - 𝗦𝗧𝗔𝗥𝗧 𝗧𝗛𝗘 𝗖𝗛𝗔𝗧𝗕𝗢𝗧
 ┏━━━━━━━━━━━━━━━━━┓
-┣★ 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 : [𝗔𝗡𝗬ᒾ⁴](https://t.me/any24e)
-┣★ 𝗦𝗨𝗣𝗣𝗢𝗥𝗧 : [𝗔𝗡𝗬ᒾ⁴](https://t.me/anymusictgvc)
+┣★ 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 : [𝗔𝗡𝗬ᒾ⁴](https://t.me/{SUPPORT_CHANNEL})
+┣★ 𝗦𝗨𝗣𝗣𝗢𝗥𝗧 : [𝗔𝗡𝗬ᒾ⁴](https://t.me/{SUPPORT_GROUP})
 ┗━━━━━━━━━━━━━━━━━┛
 
-❤️𝗜𝗙 𝗬𝗢𝗨 𝗛𝗔𝗩𝗘 𝗔𝗡𝗬 𝗨𝗘𝗦𝗧𝗜𝗢𝗡𝗦 𝗧𝗛𝗘𝗡 𝗣𝗠 𝗧𝗢 𝗠𝗬 [𝗢𝗪𝗡𝗘𝗥](https://t.me/SLDOOZY)
+❤️𝗜𝗙 𝗬𝗢𝗨 𝗛𝗔𝗩𝗘 𝗔𝗡𝗬 𝗨𝗘𝗦𝗧𝗜𝗢𝗡𝗦 𝗧𝗛𝗘𝗡 𝗣𝗠 𝗧𝗢 𝗠𝗬 [𝗢𝗪𝗡𝗘𝗥](https://t.me/{OWNER_USERNAME})
 ━━━━━━━━━━━━━━━━━━━━━━━━**""",
     reply_markup=InlineKeyboardMarkup(
             [
